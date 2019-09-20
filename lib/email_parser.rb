f2e9851e8187f398711ab53d5078ab3bce511cea
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 18:35:39 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,17 +1,5 @@
 class EmailAddressParser
 
-  attr_accessor :emails
-
-  def initialize(emails)
-    @emails = emails
-  end
-
-  def parse
-    email_array = []
-    email_array = @emails.split(/[,\s]/)
-    email_array = email_array.reject { |e| e == ""}
-    email_array.uniq
-  end
 
 end
 
