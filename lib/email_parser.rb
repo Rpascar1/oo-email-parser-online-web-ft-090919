27de15ebb0f4b91172b42806ccc1a45b7128ddad
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-20 18:53:07 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailAddressParser
     def parse
       @email_addresses = @email_addresses.strip
       @email_addresses.split(",")
-      
+      @email_addresses.collect{|x| x.strip || x}
     end  
     
     
